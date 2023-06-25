9ff72f366ba40de19ca5e666bf44842b5ca26360
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2023-06-25 20:50:18 +0900

    BST search

--- bst/main.go
+++ bst/main.go
@@ -32,10 +32,31 @@ func preOrder(n *Node) {
 	preOrder(n.Right)
 }
 
+func (n *Node) search(key int) *Node {
+	// キーが見つかった場合、現在のノードを返す
+	if n == nil || n.Key == key {
+		return n
+	}
+	// キーが現在のノードのキーより小さい場合、左の子ノードを探索
+	if key < n.Key {
+		return n.Left.search(key)
+	}
+	// それ以外の場合、右の子ノードを探索
+	return n.Right.search(key)
+}
+
 func main() {
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8}
 	root := sortedArrayToBST(arr)
 
 	fmt.Println("PreOrder traversal of constructed BST:")
 	preOrder(root)
+	fmt.Println()
+
+	result := root.search(5)
+	if result != nil {
+		fmt.Println("Found node with key:", result.Key)
+	} else {
+		fmt.Println("Key not found")
+	}
 }
