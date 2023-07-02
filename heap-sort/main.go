3a507df92061fe004fe515ee3e448a19da687f53
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2023-07-02 13:20:38 +0900

    Heap Sort

---
+++ heap-sort/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func heapify(arr []int, n int, i int) {
+	largest := i
+	left := 2*i + 1
+	right := 2*i + 2
+
+	if left < n && arr[left] > arr[largest] {
+		largest = left
+	}
+
+	if right < n && arr[right] > arr[largest] {
+		largest = right
+	}
+
+	if largest != i {
+		arr[i], arr[largest] = arr[largest], arr[i]
+		heapify(arr, n, largest)
+	}
+}
+
+func heapSort(arr []int) {
+	n := len(arr)
+
+	for i := n/2 - 1; i >= 0; i-- {
+		heapify(arr, n, i)
+	}
+
+	for i := n - 1; i > 0; i-- {
+		arr[0], arr[i] = arr[i], arr[0]
+		heapify(arr, i, 0)
+	}
+}
+
+func main() {
+	arr := []int{64, 34, 25, 12, 22, 11, 90}
+	fmt.Println("Unsorted array:")
+	fmt.Println(arr)
+
+	heapSort(arr)
+	fmt.Println("Sorted array:")
+	fmt.Println(arr)
+}
